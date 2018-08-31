f79fb9f93b9259c035ae0304595f488a4d47dfdf
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-31 01:25:12 -0700

    passing click methods to functional component using arrow funcs and bind this styles

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component {
     ]
   }
 
-  handleChangeNames = () => {
+  handleChangeNames = (newName) => {
     this.setState({
       persons: [
-        { name: "Maxi", age: 29 },
+        { name: newName, age: 29 },
         { name: "Manui", age: 30 },
         { name: "Steph", age: 27 }
       ]
@@ -26,10 +26,23 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.handleChangeNames} >Change Names</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.handleChangeNames("Maxi")} >Change Names</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.handleChangeNames.bind(this, "Max!")}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.handleChangeNames.bind(this, "Manu!")}
+        >My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+          click={this.handleChangeNames.bind(this, "Stephanie!")}
+        />
       </div>
     );
   }
