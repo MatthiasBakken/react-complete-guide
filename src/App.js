1f63aa03c2144924d9ceff155cc01a5730721c25
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-31 01:08:43 -0700

    using onClick event handler along with setState method to change props in component

--- src/App.js
+++ src/App.js
@@ -12,9 +12,13 @@ class App extends Component {
     ]
   }
 
-  handleNameChange = () => {
+  handleChangeNames = () => {
     this.setState({
-      
+      persons: [
+        { name: "Maxi", age: 29 },
+        { name: "Manui", age: 30 },
+        { name: "Steph", age: 27 }
+      ]
     })
   }
 
@@ -22,7 +26,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <button onClick={this.handleNameChange} >Switch Name</button>
+        <button onClick={this.handleChangeNames} >Change Names</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
