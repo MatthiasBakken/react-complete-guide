376c643c6f0aca3bbb76eb36e49bccebef0b67c1
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-31 00:34:11 -0700

    Using props.children

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <Person name="Max" age="28" />
-        <Person name="Manu" age="29" />
+        <Person name="Manu" age="29" >My Hobbies: Racing</Person>
         <Person name="Stephanie" age="26" />
       </div>
     );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Person = (props) => {
   return (
     <div>
-      I'm {props.name} and I am {props.age} years old
+      <p>I'm {props.name} and I am {props.age} years old</p>
+      <p>{props.children}</p>
     </div>
   );
 }
