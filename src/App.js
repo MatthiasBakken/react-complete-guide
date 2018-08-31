5a06fe28f625811db365cfd33d1042496da99003
Matthias Bakken <matthias.bakken@gmail.com>
2018-08-31 00:32:07 -0700

    using props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import Person from './Person/Person';
 import './App.css';
 
 class App extends Component {
@@ -6,6 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29" />
+        <Person name="Stephanie" age="26" />
       </div>
     );
   }
